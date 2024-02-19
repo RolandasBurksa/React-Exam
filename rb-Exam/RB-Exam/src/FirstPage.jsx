98bed2c80747cc66b78f6a5fb8a8eea11510a233
RolandasBurksa <rburksa@gmail.com>
2024-02-19 20:31:05 +0200

    Second commit

--- rb-Exam/RB-Exam/src/FirstPage.jsx
+++ rb-Exam/RB-Exam/src/FirstPage.jsx
@@ -1,21 +1,38 @@
+import { useEffect, useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import Placeholder from 'react-bootstrap/Placeholder';
+import axios from 'axios';
 
 export const FirstPage = () => {
+  const [books, setBooks] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  // parsiunciame duomenis is serverio, formuojame 'users' masyva su 'get'
+  useEffect(() => {
+    axios
+      .get('https://65d1faa1987977636bfbc142.mockapi.io/api/react-test/books')
+      .then((response) => {
+        setBooks(response.data.books);
+        setIsLoading(false);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
+  if (isLoading) {
+    return <div>Loading ...</div>;
+  }
+
   return (
     <div className="d-flex justify-content-around">
       <Card style={{ width: '18rem' }}>
         <Card.Img variant="top" src="holder.js/100px180" />
         <Card.Body>
-          <Placeholder as={Card.Title} animation="glow">
-            <Placeholder xs={6} />
-          </Placeholder>
-          <Placeholder as={Card.Text} animation="glow">
-            <Placeholder xs={7} /> <Placeholder xs={4} /> <Placeholder xs={4} /> <Placeholder xs={6} />{' '}
-            <Placeholder xs={8} />
-          </Placeholder>
-          <Placeholder.Button variant="primary" xs={6} />
+          <Card.Title>Title:</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the bulk of the card's content.
+          </Card.Text>
+          <Button variant="primary">Go somewhere</Button>
         </Card.Body>
       </Card>
     </div>
--- rb-Exam/RB-Exam/src/SecondPage.jsx
+++ rb-Exam/RB-Exam/src/SecondPage.jsx
@@ -1,7 +1,51 @@
+import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+
 export const SecondPage = () => {
+  const [inputForm, setInputForm] = useState({
+    title: '',
+    author: '',
+    category: '',
+    price: '',
+    cover: '',
+  });
+
+  const handleForm = (e) => {
+    setInputForm((prevFormData) => ({ ...prevFormData, [e.target.name]: e.target.value }));
+  };
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <>
       <h1>Second page to exam</h1>
+
+      <div>
+        <form onSubmit={handleFormSubmit}>
+          <label>
+            Title:
+            <input type="text" name="title" value={inputForm.title} onChange={handleForm} />
+          </label>
+          <label>
+            Author:
+            <input type="text" name="author" value={inputForm.author} onChange={handleForm} />
+          </label>
+          <label>
+            Category:
+            <input type="number" name="category" value={inputForm.category} onChange={handleForm} />
+          </label>
+          <label>
+            Price:
+            <input type="text" name="price" value={inputForm.price} onChange={handleForm} />
+          </label>
+          <label>
+            Cover:
+            <input type="text" name="cover" value={inputForm.cover} onChange={handleForm} />
+          </label>
+          <button type="submit">Submit</button>
+        </form>
+      </div>
     </>
   );
 };
